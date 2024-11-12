e8d0295d0030a59f0515ed2447a39e78a15ef14c
Gerald <mcamanor@gmail.com>
2024-11-12 00:27:22 +0000

    Build fixes -2

--- src/app/app/user/explore/page.tsx
+++ src/app/app/user/explore/page.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react'
 import Donations from '@/components/Donations'
+
 export default function Explore() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
--- src/components/donations.tsx
+++ src/components/donations.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import ImageCard from "./ui/image-card";
