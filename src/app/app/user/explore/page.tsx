cdcf049c9dc47cdc72ab7c6744fc616f54165ff9
Gerald <mcamanor@gmail.com>
2024-11-12 00:23:01 +0000

    Build fixes

--- src/app/app/user/explore/page.tsx
+++ src/app/app/user/explore/page.tsx
@@ -1,6 +1,5 @@
-import Donations from '@/components/Donations'
 import React, { Suspense } from 'react'
-
+import Donations from '@/components/Donations'
 export default function Explore() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
