91491015167975e6abbd5ce876c4848b907c3f00
Gerald <mcamanor@gmail.com>
2024-11-15 12:14:10 +0000

    Updated homepage meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ import { authConfig } from "@/firebase/config/server-config";
 
 export const metadata: Metadata = {
   title: "Givny",
-  description: "",
+  description: "Discover Givny.com, a platform connecting donors and seekers of pre-loved items. Easily list items you want to give away or browse available listings by category, location, and more. Create an account to manage your profile, save items, and connect with listers through messages. Whether you’re donating or finding something new, Givny makes it simple and community-driven.",
 };
 
 export default async function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { LandingPage } from "@/components/LandingPage";
 import { Suspense } from "react";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Givny",
-  description: "Discover Givny.com, a platform connecting donors and seekers of pre-loved items. Easily list items you want to give away or browse available listings by category, location, and more. Create an account to manage your profile, save items, and connect with listers through messages. Whether you’re donating or finding something new, Givny makes it simple and community-driven.",
-};
 
 export default async function Home() {
   return (
